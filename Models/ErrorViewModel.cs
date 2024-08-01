9a6fcbe83ee798052b50982ea72eeb8a49b6fd2f
Yuri Costa <yurircostawork@gmail.com>
2024-08-01 16:14:30 -0300

    Project created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
